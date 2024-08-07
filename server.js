34e2aff9b57ff1bcc733770028d25e6f6b4b2623
Julian Gelvez <julian.gelvez@tecactiva.com>
2024-08-06 19:35:16 -0500

    hola

---
+++ server.js
@@ -0,0 +1,28 @@
+//console.log("hola!");
+
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+var corsOptions = {
+    origin: 'http://localhost:8088'
+}
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:true}));
+
+app.get('/', (req, res) => {
+    res.json({message: 'Hello there'})
+});
+
+const PORT =  process.eventNames.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}.`)   
+});
+
