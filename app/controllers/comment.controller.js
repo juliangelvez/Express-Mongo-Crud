acd1af6bf888f9f4799638b07646c092dc6d67fe
Julian Gelvez <julian.gelvez@tecactiva.com>
2024-08-06 20:55:07 -0500

    command-controller

---
+++ app/controllers/comment.controller.js
@@ -0,0 +1,42 @@
+//CRUD Definitions
+
+//creare
+//findAll
+//findOne
+//Update
+//delete
+//deleteAll
+//findAllPublished
+
+const db = require("../models")
+const Comment = db.comments;
+
+//Create and Save a new comment
+
+exports.create = (req, res) => {
+
+};
+
+//Retrieve all comments from DB
+
+exports.findAll = (req, res) => {
+
+}
+
+//Find a single comment
+exports.findOne = (req, res) => {
+
+}//Delete a comment
+
+exports.delete = (req, res) => {
+
+}
+
+//Delete all comments
+exports.deleteAll = (req, res) => {
+
+}
+//Find All comments
+exports.findAllPublished = (req, res) => {
+
+}
